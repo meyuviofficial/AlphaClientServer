8b410f2257e5ee1195a6a7d6347b95c731a6fd7d
Yuvi <yuvarajselva@hotmail.com>
2022-07-10 13:31:32 +0530

    feat: Initialized the Server

--- src/main.go
+++ src/main.go
@@ -1,22 +1,23 @@
 package main
 
-import(
+import (
 	"fmt"
 	"net/http"
 	"github.com/gin-gonic/gin"
 )
 
-func main()  {
-	http.HandleFunc("/", handler)
-	err := http.ListenAndServe(":8080", nil)
-
-	if err != nil{
-		fmt.Printf("Server Closed")
-	}else {
-		fmt.Printf("Server is running")
-	}
+type Server struct {
+	PERSON string `json:"person"`
+	SERVER string `json:"server"`
+	IP string `json:"ip"`
+}
+func main() {
+	router := gin.Default()
+	router.GET("/", ApacheServer)
+	router.Run(":8080")
 }
 
-func handler(w http.ResponseWriter, r *http.Request){
-	fmt.Fprintf(w, "Hi This is Yuvi !!")
+func ApacheServer(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, Server{"Yuvi", "Server_A", "192.168.0.1"})
+	fmt.Print("Hi ! I'm running inside the server")
 }
