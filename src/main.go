42e8eba86cc9f6d8d1ce5ad988deb6345d9421c1
Yuvi <yuvarajselva@hotmail.com>
2022-07-10 14:21:29 +0530

    feat: Added the code to add elements into the global array

--- src/main.go
+++ src/main.go
@@ -3,21 +3,36 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"net/url"
+
 	"github.com/gin-gonic/gin"
 )
 
 type Server struct {
 	PERSON string `json:"person"`
-	SERVER string `json:"server"`
+	NAME string `json:"name"`
 	IP string `json:"ip"`
 }
+
+var Status[] Server
 func main() {
 	router := gin.Default()
 	router.GET("/", ApacheServer)
+	router.POST("/SSH", PostServerDetails )
 	router.Run(":8080")
 }
 
 func ApacheServer(c *gin.Context) {
-	c.IndentedJSON(http.StatusOK, Server{"Yuvi", "Server_A", "192.168.0.1"})
+	c.IndentedJSON(http.StatusOK, Status)
 	fmt.Print("Hi ! I'm running inside the server")
+}
+
+func PostServerDetails(c *gin.Context) {
+	var NewServer Server
+	c.BindJSON(&NewServer)
+	Status = append(Status, NewServer)
+	q := url.Values{}
+	location := url.URL{Path: "/", RawQuery: q.Encode()}
+
+	c.Redirect(http.StatusFound, location.RequestURI())
 }
