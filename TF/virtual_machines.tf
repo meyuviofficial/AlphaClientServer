2d45289753413a1909a09a55099bc938beb13d54
Yuvi <yuvarajselva@hotmail.com>
2022-07-14 22:40:21 +0530

    fix: Corrected the resource group name for the VNET

--- TF/virtual_machines.tf
+++ TF/virtual_machines.tf
@@ -10,7 +10,7 @@ resource "azurerm_virtual_network" "AlphaClientVNET" {
 
 resource "azurerm_subnet" "AlphaClientSubnet" {
   name                 = var.Subnet
-  resource_group_name  = azurerm_resource_group.AlphaClientServer.location
+  resource_group_name  = azurerm_resource_group.AlphaClientServer.name
   virtual_network_name = azurerm_virtual_network.AlphaClientVNET.name
   address_prefixes     = ["10.0.2.0/24"]
 }
