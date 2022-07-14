fa16a0d644ed2331bf968a274e8e74695d5eae51
Yuvi <yuvarajselva@hotmail.com>
2022-07-14 07:50:10 +0530

    feat: Added the content type as plain/text

--- src/main.go
+++ src/main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"net/http"
 	"net/url"
+
 	"github.com/gin-gonic/gin"
 )
 
@@ -16,7 +18,7 @@ type Server struct {
 var Status map[Server]int = make(map[Server]int, 0)
 
 func main() {
-	// Status =  make(map[Server]int, 0)
+	Status =  make(map[Server]int, 0)
 	router := gin.Default()
 	router.GET("/", AlphaServer)
 	router.POST("/SSH", PostServerDetails )
@@ -25,19 +27,24 @@ func main() {
 
 func AlphaServer(c *gin.Context) {
 
-	var Response[] string
-	fmt.Print(Status)
+	var Response bytes.Buffer
+	header := c.Writer.Header()
+	header["Content-Type"] = []string{"text/plain"}
+	c.Writer.WriteHeaderNow()
+
+	// fmt.Print(Status)
 	for CurrentServer, LoginCount := range Status {
-		fmt.Printf("Server : %v was logged in by %v for %v number of times \n", CurrentServer.NAME, CurrentServer.PERSON, LoginCount)
-		CurrentOutput := fmt.Sprintf("Server : %v was logged in by %v for %v number of times \n", CurrentServer.NAME, CurrentServer.PERSON, LoginCount)
-		Response = append(Response, string(CurrentOutput))
+		// fmt.Printf("Server : %v was logged in by %v for %v number of times", CurrentServer.NAME, CurrentServer.PERSON, LoginCount)
+		CurrentOutput := fmt.Sprintf("Server : %v was logged in by %v for %v number of times\n", CurrentServer.NAME, CurrentServer.PERSON, LoginCount)
+		Response.WriteString(CurrentOutput)
+		// Response.WriteString("</br>")
 	}
 	
-	if Response != nil {
-		c.IndentedJSON(http.StatusOK, Response)
+	if Response.String() != "" {
+		c.String(http.StatusAccepted, Response.String())
 	} else {
 		// Response = append(Response, "OOPS !! No Data yet. Please SSH into some machines to get the data")
-		c.IndentedJSON(http.StatusOK, "OOPS !! No Data yet. Please SSH into some machines to get the data")
+		c.String(http.StatusOK, "OOPS !! No Data yet. Please SSH into some machines to get the data")
 	}
 }
 
--- src/main_test.go
+++ src/main_test.go
@@ -19,7 +19,7 @@ func SetUpRouter() *gin.Engine{
     return router
 }
 func TestDefaultPage(t *testing.T) {
-    mockResponse := `"OOPS !! No Data yet. Please SSH into some machines to get the data"`
+    mockResponse := "OOPS !! No Data yet. Please SSH into some machines to get the data"
     r := SetUpRouter()
     r.GET("/", AlphaServer)
     req, _ := http.NewRequest("GET", "/", nil)
@@ -47,4 +47,21 @@ func TestPostMethod(t *testing.T) {
 	w := httptest.NewRecorder()
 	r.ServeHTTP(w, req)
 	assert.Equal(t, http.StatusFound, w.Code)
+
+// func TestBadRequest(t *testing.T) {
+// 	r := SetUpRouter()
+// 	r.POST("/SSH", PostServerDetails)
+	
+// 	newServer := Server{
+// 		PERSON: "Person A",
+// 		NAME: "Server A",
+// 		IP: "192.168.0.1",
+// 	}
+
+// 	jsonBody, _ := json.Marshal(newServer)
+// 	// fmt.Print(jsonBody)
+// 	req, _ := http.NewRequest("POST", "/SSH", bytes.NewBuffer(jsonBody))
+// 	w := httptest.NewRecorder()
+// 	r.ServeHTTP(w, req)
+// 	assert.Equal(t, http.StatusFound, w.Code)
 }
