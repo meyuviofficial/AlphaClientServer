6e727abfe098dffeaa2c3b329f7f44cd65bcf348
Yuvi <yuvarajselva@hotmail.com>
2022-07-14 21:28:06 +0530

    test: added the test case for the badrequest

--- src/main_test.go
+++ src/main_test.go
@@ -3,42 +3,39 @@ package main
 import (
 	"bytes"
 	"encoding/json"
-	// "fmt"
+	"github.com/gin-gonic/gin"
+	"github.com/stretchr/testify/assert"
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
 	"testing"
-
-	// "github.com/stretchr/testify"
-	"github.com/gin-gonic/gin"
-	"github.com/stretchr/testify/assert"
 )
 
-func SetUpRouter() *gin.Engine{
-    router := gin.Default()
-    return router
+func SetUpRouter() *gin.Engine {
+	router := gin.Default()
+	return router
 }
 func TestDefaultPage(t *testing.T) {
-    mockResponse := "OOPS !! No Data yet. Please SSH into some machines to get the data"
-    r := SetUpRouter()
-    r.GET("/", AlphaServer)
-    req, _ := http.NewRequest("GET", "/", nil)
-    w := httptest.NewRecorder()
-    r.ServeHTTP(w, req)
+	mockResponse := "OOPS !! No Data yet. Please SSH into some machines to get the data"
+	r := SetUpRouter()
+	r.GET("/", AlphaServer)
+	req, _ := http.NewRequest("GET", "/", nil)
+	w := httptest.NewRecorder()
+	r.ServeHTTP(w, req)
 
-    responseData, _ := ioutil.ReadAll(w.Body)
-    assert.Equal(t, mockResponse, string(responseData))
-    assert.Equal(t, http.StatusOK, w.Code)
+	responseData, _ := ioutil.ReadAll(w.Body)
+	assert.Equal(t, mockResponse, string(responseData))
+	assert.Equal(t, http.StatusOK, w.Code)
 }
 
 func TestPostMethod(t *testing.T) {
 	r := SetUpRouter()
-    r.POST("/SSH", PostServerDetails)
-	
+	r.POST("/SSH", PostServerDetails)
+
 	newServer := Server{
-		PERSON: "Person A",
-		NAME: "Server A",
-		IP: "192.168.0.1",
+		User:       "Person A",
+		ServerName: "Server A",
+		Ip:         "192.168.0.1",
 	}
 
 	jsonBody, _ := json.Marshal(newServer)
@@ -47,5 +44,24 @@ func TestPostMethod(t *testing.T) {
 	w := httptest.NewRecorder()
 	r.ServeHTTP(w, req)
 	assert.Equal(t, http.StatusFound, w.Code)
+
+}
+
+func TestBadRequest(t *testing.T) {
+	r := SetUpRouter()
+	r.POST("/SSH", PostServerDetails)
+
+	type MockServer struct {
+		Info string `json:"info" binding:"required"`
+	}
+
+	MockServerInfo := MockServer{
+		Info: "MockValue",
+	}
+	jsonBody, _ := json.Marshal(MockServerInfo)
+	req, _ := http.NewRequest("POST", "/SSH", bytes.NewBuffer(jsonBody))
+	w := httptest.NewRecorder()
+	r.ServeHTTP(w, req)
+	assert.Equal(t, http.StatusBadRequest, w.Code)
 
 }
