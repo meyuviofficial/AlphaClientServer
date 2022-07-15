98a41b739ed70b7a4ef8ae369d5d721dc7e7aba2
Yuvi <yuvarajselva@hotmail.com>
2022-07-15 05:44:00 +0530

    feat: Added the code for the Alpha Client as a Script

---
+++ scripts/alphaclient.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+servername=$(hostname)
+ipaddress=$(hostname -I)
+useradmin=$(whoami)
+
+body=$(cat <<EOF
+{
+"user": "$useradmin",
+"servername": "$servername",
+"ip": "$ipaddress"
+}
+EOF
+)
+
+
+    curl http://20.253.162.142/SSH \
+        -v -i -L\
+        -X POST \
+        -H "Accept: application/json" \
+        -H "Content-Type:application/json" \
+        -d "$body"
